2ee19c82a2fca5097cad8ff16c94b1ee088955e1
Daniel Milnes <thebeanogamer@gmail.com>
2020-03-25 21:08:09 +0000

    Fix quote retrieve

--- quoteswebsite/static/quotes.js
+++ quoteswebsite/static/quotes.js
@@ -1,12 +1,13 @@
 (function quotes() {
-    quote = $.getJSON("/api/quote");
-    $("#quote").text(quote.text);
-    $("#author").text("- " + quote.author);
-    $("#permalink_input").val(window.location.hostname + "/quote/" + quote.id);
+    $.getJSON("/api/quote", function(quote){
+            $("#quote").text(quote.text);
+            $("#author").text("- " + quote.author);
+            $("#permalink_input").val(window.location.hostname + "/quote/" + quote.id);
 
-    $("#permalink_button").click(function () {
-        input = $("#permalink_input");
-        input.select();
-        document.execCommand("copy");
+            $("#permalink_button").click(function () {
+                input = $("#permalink_input");
+                input.select();
+                document.execCommand("copy");
+            });
     });
 })();
