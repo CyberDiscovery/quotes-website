d0f4bca6e7989a311d11d790d69aefcc7b91d6f2
Daniel Milnes <thebeanogamer@gmail.com>
2020-04-05 18:06:46 +0100

    Specify protocol

--- quoteswebsite/static/quotes.js
+++ quoteswebsite/static/quotes.js
@@ -2,7 +2,7 @@
     $.getJSON("/api/quote", function(quote){
             $("#quote").text(quote.quote);
             $("#author").text("- " + quote.author);
-            $("#permalink_input").val(window.location.hostname + "/quote/" + quote.id);
+            $("#permalink_input").val("https://" + window.location.hostname + "/quote/" + quote.id);
 
             $("#permalink_button").click(function () {
                 input = $("#permalink_input");
